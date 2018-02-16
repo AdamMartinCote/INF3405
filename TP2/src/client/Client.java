396dca22537e4740a1d2b0f165f313a36bca4ecd
Adam <adam.martin-cote@polymtl.ca>
2018-02-16 10:08:13 -0500

    Client gets ip+port and load image

--- TP2/src/client/Client.java
+++ TP2/src/client/Client.java
@@ -1,113 +1,61 @@
 package client;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.Scanner;
 
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
 import utils.Utils;
 
 public class Client {
+	private final static String FILE_PATH = "/home/void/code/tp/INF3405/TP2/src/lassonde.jpg";
 
     private BufferedReader in;
     private PrintWriter out;
-    private JFrame frame = new JFrame("Capitalize Client");
-    private JTextField dataField = new JTextField(40);
-    private JTextArea messageArea = new JTextArea(8, 60);
-    
 
-    /**
-     * Constructs the client by laying out the GUI and registering a
-     * listener with the textfield so that pressing Enter in the
-     * listener sends the textfield contents to the server.
-     */
+    
     Client() {
 
-        // Layout GUI
-        messageArea.setEditable(false);
-        frame.getContentPane().add(dataField, "North");
-        frame.getContentPane().add(new JScrollPane(messageArea), "Center");
-
-        // Add Listeners
-        dataField.addActionListener(new ActionListener() {
-            /**
-             * Responds to pressing the enter key in the textfield
-             * by sending the contents of the text field to the
-             * server and displaying the response from the server
-             * in the text area.  If the response is "." we exit
-             * the whole application, which closes all sockets,
-             * streams and windows.
-             */
-            public void actionPerformed(ActionEvent e) {
-                out.println(dataField.getText());
-                   String response;
-                try {
-                    response = in.readLine();
-                    if (response == null || response.equals("")) {
-                          System.exit(0);
-                      }
-                } catch (IOException ex) {
-                       response = "Error: " + ex;
-                }
-                messageArea.append(response + "\n");
-                dataField.selectAll();
-            }
-        });
     }
 
-    /**
-     * Implements the connection logic by prompting the end user for
-     * the server's IP address, connecting, setting up streams, and
-     * consuming the welcome messages from the server.  The Capitalizer
-     * protocol says that the server sends three lines of text to the
-     * client immediately after establishing a connection.
-     */
     @SuppressWarnings("resource")
 	public void connectToServer() throws IOException {
 
         // Get the server address from a dialog box.
-        String serverAddress = JOptionPane.showInputDialog(frame,"Enter IP Address of the Server:","Welcome to the Capitalization Program",JOptionPane.QUESTION_MESSAGE);
-        int port = 5000;
+        String serverAddress = Utils.getValidIpFromUser();
+        int port = Utils.getValidPortFromUser();
 
         Socket socket;
 		socket = new Socket(serverAddress, port);
 		
-        System.out.format("The capitalization server is running on %s:%d%n", serverAddress, port);
+        System.out.format("Connected to %s:%d%n", serverAddress, port);
         
-        in = new BufferedReader(
-                new InputStreamReader(socket.getInputStream()));
-        out = new PrintWriter(socket.getOutputStream(), true);
+//        in = new BufferedReader(
+//                new InputStreamReader(socket.getInputStream()));
+//        out = new PrintWriter(socket.getOutputStream(), true);
 
-        // Consume the initial welcoming messages from the server
-        for (int i = 0; i < 3; i++) {
-            messageArea.append(in.readLine() + "\n");
-        }
     }
 
-    /**
-     * Runs the client application.
-     */
     public static void main(String[] args) throws Exception {
     	
-    	System.out.println("Please enter ip address: ");
-    	
+        FileInputStream fis = null;
+        BufferedInputStream bis = null;
+        
         Client client = new Client();
-        client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        client.frame.pack();
-        client.frame.setVisible(true);
         try {
         	client.connectToServer();
         } catch(Exception e) {
         	System.out.println("Error while connecting -- " + e.getMessage());
         }
+//        System.out.println("Please select a file");
+        File imageToSend = new File(FILE_PATH);
+        
+        byte [] mybytearray  = new byte [(int)imageToSend.length()];
+        fis = new FileInputStream(imageToSend);
+        bis = new BufferedInputStream(fis);
+        bis.read(mybytearray,0,mybytearray.length);
     }
 }
